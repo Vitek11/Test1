be824dbbab4af28d1873768935bc1907eae268e2
Vitek11 <vkir28@gmail.com>
2022-06-04 14:28:45 +0300

    sqrt1

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Hello , my dear friend!")
@@ -10,4 +13,14 @@ func main() {
 	s = s1 + s2
 	fmt.Println(s)
 
+	var x, y int32
+	var z1, z2 float64
+
+	x = 25
+	y = 42
+
+	z1 = math.Sqrt(float64(x))
+	z2 = math.Sqrt(float64(y))
+
+	fmt.Println(z1, z2)
 }
