386a89fd5174f0b1666554aca698d59f2dca152b
Vitek11 <vkir28@gmail.com>
2022-06-04 09:53:19 +0300

    string_add

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello , my dear friend!")
-	var s string = "How are you?"
+	var s, s1, s2 string
+	s1 = "How"
+	s2 = " are you?"
+	s = s1 + s2
 	fmt.Println(s)
 
 }
